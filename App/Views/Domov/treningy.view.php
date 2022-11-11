7d6c558faa21283e68029d1e57718a8d08a66db8
Maroš Gorný <maros.gorny@gmail.com>
2022-11-11 21:07:13 +0100

    add VAII framework
    add database

---
+++ App/Views/Domov/treningy.view.php
@@ -0,0 +1,95 @@
+<section class="container-fluid px-0">
+    <div class="row align-items-center">
+        <div class="col-lg-6 home-block">
+            <img class="img-fluid"
+                 src="../../../public/photos/osobnyTrener.jpg"
+                 alt="Trainer with client">
+        </div>
+        <div class="col-lg-6 home-block">
+            <div id="headingGroup" class="text-center d-none d-lg-block">
+                <h1 class="">TRÉNINGY</h1>
+                <h1 class="">TRÉNINGY</h1>
+                <h1 class="">TRÉNINGY</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container-fluid px-0">
+    <div class="row align-items-center content">
+
+        <div class="col-md-8 text-center">
+            <div class="row justify-content-center">
+                <div class="col-10 col-lg-8  mb-0 home-block home-page-text">
+                    <h2>Skupinové individuálne tréningy</h2>
+                    <p class="lead">Individuálne tréningy s naším trénerom si viete dohodnúť aj ako skupinka.</p>
+                    <h2>Samostatné individuálne tréningy</h2>
+                    <p class="lead">Výhodou pri osobných tréningoch je, že v priestore sa nachádzate iba vy a tréner.</p>
+                </div>
+                <a class="btn btn-secondary btn-lg btn-block rounded-0" href="#" role="button">Prihlásiť sa</a>
+            </div>
+        </div>
+
+        <div class="col-md-4 col-lg-4 d-none d-md-flex d-lg-flex home-block ">
+            <img class="img-fluid float-right"
+                 src="../../../public/photos/osobnyTrenerSkupina.jpg"
+                 alt="Box punch">
+        </div>
+
+    </div>
+
+    <div class="row align-items-center content">
+
+        <div class="col-md-8 text-center order-1 order-md-2 home-block">
+            <div class="row justify-content-center ">
+                <div class="col-10 col-lg-8 mb-0 home-page-text">
+                    <h2>Silove treningy</h2>
+                    <p class="lead">Silove treningy su vyborne na nabratie sily a zvacsanie svalov.</p>
+                </div>
+                <a class="btn btn-secondary btn-lg btn-block rounded-0" href="#" role="button">Prihlásiť sa</a>
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-4 d-none d-md-flex d-lg-flex home-block order-md-1">
+            <img class="img-fluid"
+                 src="../../../public/photos/benchpress.jpg"
+                 alt="Trainer with clients">
+        </div>
+    </div>
+
+    <div class="row align-items-center content">
+
+        <div class="col-md-8 text-center">
+            <div class="row justify-content-center">
+                <div class="col-10 col-lg-8  mb-0 home-block home-page-text">
+                    <h2>Kondicne treningy</h2>
+                    <p class="lead">Kondicne treningy su vhodne na chudnutie, rysovanie tela alebo zlepsenie si kondicky.</p>
+                </div>
+                <a class="btn btn-secondary btn-lg btn-block rounded-0" href="#" role="button">Prihlásiť sa</a>
+            </div>
+        </div>
+
+        <div class="col-md-4 col-lg-4 d-none d-md-flex d-lg-flex home-block">
+            <img class="img-fluid"
+                 src="../../../public/photos/paste.jpg"
+                 alt="Training room">
+        </div>
+    </div>
+
+    <div class="row align-items-center content">
+
+        <div class="col-md-8 text-center order-1 order-md-2 ">
+            <div class="row justify-content-center ">
+                <div class="col-10 col-lg-8 mb-0 home-page-text">
+                    <h2>Funkcne treningy</h2>
+                    <p class="lead">Funkcne treningy sluzia na lepsiu stabilitu a koordinaciu celeho tela.</p>
+                </div>
+                <a class="btn btn-secondary btn-lg btn-block rounded-0" href="#" role="button">Prihlásiť sa</a>
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-4 d-none d-md-flex d-lg-flex home-block order-md-1">
+            <img class="img-fluid"
+                 src="../../../public/photos/stojka.jpg"
+                 alt="Trainer with clients">
+        </div>
+    </div>
+</section>
