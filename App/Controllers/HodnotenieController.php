efeb95e12d987002fd97dc7b10b1f0edfebbf1cb
Maroš Gorný <maros.gorny@gmail.com>
2023-01-12 21:12:15 +0100

    add link to all trainings info

--- App/Controllers/HodnotenieController.php
+++ App/Controllers/HodnotenieController.php
@@ -22,6 +22,24 @@ class HodnotenieController extends AControllerBase
         return $this->html(['Ind_Sku_trening' => $hodnotenia_ind_sku]);
     }
 
+    public function silovyTrening(): Response
+    {
+        $hodnotenia_silovy = Hodnotenie::getAll(whereClause: "topic = 'Sil_trening'");
+        return $this->html(['Sil_trening' => $hodnotenia_silovy]);
+    }
+
+    public function kondicnyTrening(): Response
+    {
+        $hodnotenia_kondicny = Hodnotenie::getAll(whereClause: "topic = 'Kon_trening'");
+        return $this->html(['Kon_trening' => $hodnotenia_kondicny]);
+    }
+
+    public function funkcnyTrening(): Response
+    {
+        $hodnotenia_funkcny = Hodnotenie::getAll(whereClause: "topic = 'Fun_trening'");
+        return $this->html(['Fun_trening' => $hodnotenia_funkcny]);
+    }
+
     public function addComment() {
         //TODO dorobit topic aby sa spravne pridal a spravny view
         return $this->html(['hodnotenie' => new Hodnotenie(),'topic' => null],viewName: 'create.form');
